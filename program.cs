f974fd8c065ac95bf058343806f60ee54f255d90
João Pedro Barbosa <160193213+BarbosaJp1@users.noreply.github.com>
2024-04-19 17:40:03 -0300

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números ímpares de 1 até 1000:");
+
+        for (int i = 1; i <= 1000; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
